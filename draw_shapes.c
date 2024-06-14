c6060cf27f5a90a9e0a064cc9d5f4fed2961ded1
jaranda4 <144184734+jaranda4@users.noreply.github.com>
2024-06-14 09:35:03 -0600

    Update draw_shapes.c for arrow

--- draw_shapes.c
+++ draw_shapes.c
@@ -25,4 +25,22 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
-
+// Prints Arrow using code from Square and Triangle
+void print_arrow(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('*');
+    putchar('\n');
+  }
+}
